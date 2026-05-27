907154e371394175b5a212c6b7a32b0a57d13c20
Aaron Gundel <124614+aarongundel@users.noreply.github.com>
2026-05-27 16:46:12 -0600

    adds arches controlled lists, modifies load hook

--- modeling_demo/urls.py
+++ modeling_demo/urls.py
@@ -14,7 +14,8 @@ handler500 = "arches.app.views.main.custom_500"
 
 # Ensure Arches core urls are superseded by project-level urls
 urlpatterns.append(path("", include("arches.urls")))
-
+urlpatterns.append(path("", include("arches_controlled_lists.urls")))
+urlpatterns.append(path("", include("arches_component_lab.urls")))
 # Adds URL pattern to serve media files during development
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
