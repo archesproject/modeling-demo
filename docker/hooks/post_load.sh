e1aaee177d57bd79dd4e98c23f3defd6d144c1d8
Aaron <124614+aarongundel@users.noreply.github.com>
2026-05-28 15:40:50 -0600

    udpates post load hook (#11)

--- docker/hooks/post_load.sh
+++ docker/hooks/post_load.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd ${WEB_ROOT} &&git clone https://github.com/archesproject/Ogee.git
 rm -rf ${WEB_ROOT}/Ogee/business_data/*
-cd ${APP_ROOT} && python3 manage.py packages -o load_package -s ../Ogee -y
+cd ${APP_ROOT} && ${WEB_ROOT}/ENV/bin/python manage.py packages -o load_package -s ../Ogee -y
+${WEB_ROOT}/ENV/bin/python manage.py controlled_lists -o migrate_collections_to_controlled_lists -co ''
